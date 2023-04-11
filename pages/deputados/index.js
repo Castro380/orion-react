1936290c9f4fbc236d7e768b965ecc75faab6a2c
Castro <mateuso@iesb.edu.br>
2023-04-11 19:35:24 -0300

    react.orion

---
+++ pages/deputados/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Pagina from '../../components/Pagina'
+
+
+const index = () => {
+  return (
+    <>
+    <Pagina > 
+    <h1>deputados</h1>
+    </Pagina>
+    </>
+  )
+}
+
+
+export default index
